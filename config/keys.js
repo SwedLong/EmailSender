2eed726e356d8ff7d9d62c5433f8571ed98adf97
Alexander Chan <alexander.chan86@gmail.com>
2019-01-24 12:34:56 +0100

    change

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === "production") {
     //we are in production require in the prd set of keys
-    module.exports = "./prod";
+    module.exports = require("./prod");
 } else {
     //we are in dev return the dev keys
     module.exports = require("./dev");
